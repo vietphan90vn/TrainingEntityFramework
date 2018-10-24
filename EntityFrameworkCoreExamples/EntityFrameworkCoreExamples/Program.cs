074530c656517fdca0f3150487c271c7637e1477
VietPhan <vietphan90vn@gmail.com>
2018-10-24 17:24:51 +0700

    Add DatabaseFirst project

---
+++ EntityFrameworkCoreExamples/EntityFrameworkCoreExamples/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
