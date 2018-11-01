2434e92c49895ef62127988a3d17ded448ac4e19
Viet Phan <viet.phanho@asnet.com.vn>
2018-11-01 23:01:02 +0700

    Add Explicit load

--- EntityFrameworkCoreExamples/DatabaseFirst/Program.cs
+++ EntityFrameworkCoreExamples/DatabaseFirst/Program.cs
@@ -24,6 +24,7 @@ namespace DatabaseFirst
 			course.ForEach(b => Console.WriteLine(b));
 
 			// Eager loading
+			Console.WriteLine("=========Eager loading=========");
 			using (var context2 = new SchoolDBContext())
 			{
 				var student = context2.Student
@@ -33,6 +34,24 @@ namespace DatabaseFirst
 					.ToList();
 
 				Console.WriteLine(student);
+			}
+
+			//Explicit load
+			Console.WriteLine("========Explicit load=========");
+			using (var context3 = new SchoolDBContext())
+			{
+				var student = context3.Student
+					.Single(s => s.StudentName.Contains("B"));
+
+				Console.WriteLine(student.StudentName);
+
+				var result = context3.Entry(student)
+					.Collection(s => s.StudentCourse)
+					.Query()
+					.ToList();
+
+				result.ForEach(a => Console.WriteLine(a.Course));
+
 			}
 		}
     }
